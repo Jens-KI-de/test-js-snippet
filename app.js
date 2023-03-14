872c1b469da8b4de18b37db944acefc01585089c
Jens-KI-de <127835372+Jens-KI-de@users.noreply.github.com>
2023-03-14 15:40:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import { accumulate } from "./snippets/accumulate.js";
 
 let sums = accumulate(1, 2, 3, 4);
-document.getElementById("test").innerHTML(sums);
+document.getElementById("accumulate-test").innerHTML(sums);
